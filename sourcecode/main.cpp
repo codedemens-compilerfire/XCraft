2ef666e802c8e26107438e5f47e9f44c9f7da23b
Dawid Jakubowski <selfashynook@gmail.com>
2026-01-30 10:41:48 +0100

    CMakeLists.txt

---
+++ sourcecode/main.cpp
@@ -0,0 +1,34 @@
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <vector>
+#include <list>
+
+int main(int argc, char* argv[]) {
+
+    sf::RenderWindow rw;
+    rw.create(sf::VideoMode(640, 480), "XCraft", sf::Style::Close);
+
+    while (rw.isOpen()) {
+        sf::Event e;
+        while (rw.pollEvent(e)) {
+            if (e.type == sf::Event::EventType::Closed) {
+                rw.close();
+            }
+        }
+
+        rw.clear(sf::Color::Black);
+        rw.display();
+    }
+
+    return 0;
+}
